0a3043fd102d07713a39fd739729ceea09434d7b
MrPekar98 <jm14ok@cs.aau.dk@a256-gc1-19.srv.aau.dk>
2023-10-23 10:44:09 +0200

    Minor refactor

--- benchmarks/benchmark.sh
+++ benchmarks/benchmark.sh
@@ -15,35 +15,35 @@ then
     mkdir -p DBpedia-2014/
     mkdir -p Wikidata/
 
-    echo "Indexing keyword-kg-linkg on DBpedia 12/2022"
+    echo "Indexing keyword-kg-linker on DBpedia 12/2022"
     /setup/kg/neo4j-dbpedia-12-2022/bin/neo4j start
     sleep 15m
     ./linker.sh -dir DBpedia-12-2022/ -predicate ${LABEL_PREDICATE}
     /setup/kg/neo4j-dbpedia-12-2022/bin/neo4j stop
     sleep 1m
 
-    echo "Indexing keyword-kg-linkg on DBpedia 03/2022"
+    echo "Indexing keyword-kg-linker on DBpedia 03/2022"
     /setup/kg/neo4j-dbpedia-03-2022/bin/neo4j start
     sleep 15m
     ./linker.sh -dir DBpedia-03-2022/ -predicate ${LABEL_PREDICATE}
     /setup/kg/neo4j-dbpedia-03-2022/bin/neo4j stop
     sleep 1m
 
-    echo "Indexing keyword-kg-linkg on DBpedia 10/2016"
+    echo "Indexing keyword-kg-linker on DBpedia 10/2016"
     /setup/kg/neo4j-dbpedia-10-2016/bin/neo4j start
     sleep 15m
     ./linker.sh -dir DBpedia-10-2016/ -predicate ${LABEL_PREDICATE}
     /setup/kg/neo4j-dbpedia-10-2016/bin/neo4j stop
     sleep 1m
 
-    echo "Indexing keyword-kg-linkg on DBpedia 2014"
+    echo "Indexing keyword-kg-linker on DBpedia 2014"
     /setup/kg/neo4j-dbpedia-2014/bin/neo4j start
     sleep√15m
     ./linker.sh -dir DBpedia-2014/ -predicate ${LABEL_PREDICATE}
     /setup/kg/neo4j-dbpedia-2014/bin/neo4j stop
     sleep 1m
 
-    echo "Indexing keyword-kg-linkg on Wikidata"
+    echo "Indexing keyword-kg-linker on Wikidata"
     /setup/kg/neo4j-wikidata/bin/neo4j start
     sleep 24h
     ./linker.sh -dir Wikidata/ -predicate ${LABEL_PREDICATE}
