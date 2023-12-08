1589426455bc5c3ea9bec4a665adb1029b23d3ae
MrPekar98 <jm14ok@cs.aau.dk@a256-gc1-19.srv.aau.dk>
2023-12-08 09:55:20 +0100

    Run Magic

--- benchmark.sh
+++ benchmark.sh
@@ -11,15 +11,15 @@ docker run --rm --network evaluation -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/
 docker stop searx
 
 # Magic
-#docker run --rm --network evaluation -d --name dbpedia-spotlight spotlight_dbp-10-2016
-#docker run --rm --network evaluation -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/results:/results magic_dbp-10-2016
-#docker stop dbpedia-spotlight
+docker run --rm --network evaluation -d --name dbpedia-spotlight spotlight_dbp-10-2016
+docker run --rm --network evaluation -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/results:/results magic_dbp-10-2016
+docker stop dbpedia-spotlight
 
-#docker run --rm --network evaluation -d --name dbpedia-spotlight spotlight_dbp-03-2022
-#docker run --rm --network evaluation -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/results:/results magic_dbp-03-2022
-#docker stop dbpedia-spotlight
+docker run --rm --network evaluation -d --name dbpedia-spotlight spotlight_dbp-03-2022
+docker run --rm --network evaluation -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/results:/results magic_dbp-03-2022
+docker stop dbpedia-spotlight
 
-#docker run --rm --network evaluation -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/results:/results magic_wd
+docker run --rm --network evaluation -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/results:/results magic_wd
 
 # Keyword-kg-linker
 
