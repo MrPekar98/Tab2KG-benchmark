dbc270fdce8532d9ba9b637b27a174d64d978de1
JM14OK <jm14ok@cs.aau.dk@a2048-gc1-02.srv.aau.dk>
2023-12-18 17:47:11 +0100

    Bug fixes

--- baselines/bbw/experiments/quality.sh
+++ baselines/bbw/experiments/quality.sh
@@ -5,16 +5,16 @@ set -e
 BENCHMARK_DIR="/benchmarks/"
 
 # SemTab HardTables
-python3 bbwWrapper.py "${BENCHMARK_DIR}semtab/HardTables/tables/" semtab_hardtables
+python bbwWrapper.py "${BENCHMARK_DIR}semtab/HardTables/tables/" semtab_hardtables
 
 # tFood
-python3 bbwWrapper.py "${BENCHMARK_DIR}semtab/tfood/horizontal/tables/" tfood
+python bbwWrapper.py "${BENCHMARK_DIR}semtab/tfood/horizontal/tables/" tfood
 
 # Tough Tables
-python3 bbwWrapper.py "${BENCHMARK_DIR}toughtables/wikidata/tables/" toughtables_wd
+python bbwWrapper.py "${BENCHMARK_DIR}toughtables/wikidata/tables/" toughtables_wd
 
 # Wikitables 2013
-python3 bbwWrapper.py "${BENCHMARK_DIR}wikitables_2013/tables_subset/" wikitables_2013
+python bbwWrapper.py "${BENCHMARK_DIR}wikitables_2013/tables_subset/" wikitables_2013
 
 # Wikitables 2019
-python3 bbwWrapper.py "${BENCHMARK_DIR}wikitables_2019/tables_subset/" wikitables_2019
+python bbwWrapper.py "${BENCHMARK_DIR}wikitables_2019/tables_subset/" wikitables_2019
