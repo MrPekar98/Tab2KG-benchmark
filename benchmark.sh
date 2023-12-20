57699dc8f4ee9d00931a99fc1798830d4cce690d
MrPekar98 <jm14ok@cs.aau.dk@a256-gc1-19.srv.aau.dk>
2023-12-20 13:52:42 +0100

    Run all experiments with LexMa

--- benchmark.sh
+++ benchmark.sh
@@ -32,3 +32,13 @@ docker run --rm --network evaluation -d -v ${PWD}/baselines/magic/tdb_dbp_2016/:
 sleep 2m
 docker run --rm --network evaluation -e ENDPOINT=${IP} -e KG=dbp_16 -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/results:/results lexma
 docker stop kg-lookup-service
+
+docker run --rm --network evaluation -d -v ${PWD}/baselines/magic/tdb_dbp_2022/:/tdb -v ${PWD}/baselines/magic/lucene_dbp_2022/:/lucene -p 7000:7000 --name kg-lookup-service kg-lookup
+sleep 2m
+docker run --rm --network evaluation -e ENDPOINT=${IP} -e KG=dbp_22 -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/results:/results lexma
+docker stop kg-lookip-service
+
+docker run --rm --network evaluation -d -v ${PWD}/baselines/lexma/tdb_wd/:/tdb -v ${PWD}/baselines/lexma/lucene_wd/:/lucene -p 7000:7000 --name kg-lookup-service kg-lookup
+sleep 2m
+docker run --rm --network evaluation -e ENDPOINT=${IP} -e KG=wd -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/results:/results lexma
+docker stop kg-lookup-service
