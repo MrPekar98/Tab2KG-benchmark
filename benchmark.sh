bf4f303bf6ddddc397139775e4a1f7db4051e128
MrPekar98 <jm14ok@cs.aau.dk@a256-gc1-19.srv.aau.dk>
2024-01-04 10:48:07 +0100

    Ignore unnecessary context

--- benchmark.sh
+++ benchmark.sh
@@ -19,15 +19,15 @@ docker stop fuseki-service
 # Magic
 docker run --rm --network evaluation -d -v ${PWD}/baselines/magic/tdb_dbp_2016/:/tdb -v ${PWD}/baselines/magic/lucene_dbp_2016/:/lucene -p 7000:7000 --name kg-lookup-service kg-lookup
 sleep 2m
-docker run --rm --network evaluation -e ENDPOINT=${IP} -e KG="dbp-10-2016" -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/baselines/magic/:/hdt -v ${PWD}/results:/results magic_dbp-10-2016
+docker run --rm --network evaluation -e ENDPOINT=${IP} -e KG="dbp-10-2016" -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/baselines/magic/:/hdt -v ${PWD}/results:/results magic
 docker stop kg-lookup-service
 
 docker run --rm --network evaluation -d -v ${PWD}/baselines/magic/tdb_dbp_2022/:/tdb -v ${PWD}/baselines/magic/lucene_dbp_2022/:/lucene -p 7000:7000 --name kg-lookup-service kg-lookup
 sleep 2m
-docker run --rm --network evaluation -e ENDPOINT=${IP} -e KG="dbp-03-2022" -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/baselines/magic/:/hdt -v ${PWD}/results:/results magic_dbp-12-2022
+docker run --rm --network evaluation -e ENDPOINT=${IP} -e KG="dbp-12-2022" -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/baselines/magic/:/hdt -v ${PWD}/results:/results magic
 docker stop kg-lookup-service
 
-docker run --rm --network evaluation -e ENDPOINT=${IP} -e KG="wd" -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/baselines/magic/:/hdt -v ${PWD}/results:/results magic_wd
+docker run --rm --network evaluation -e ENDPOINT=${IP} -e KG="wd" -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/baselines/magic/:/hdt -v ${PWD}/results:/results magic
 
 # LexMa
 docker run --rm --network evaluation -d -v ${PWD}/baselines/magic/tdb_dbp_2016/:/tdb -v ${PWD}/baselines/magic/lucene_dbp_2016/:/lucene -p 7000:7000 --name kg-lookup-service kg-lookup
