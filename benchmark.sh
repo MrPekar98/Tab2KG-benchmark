87580986085ff44fe4462f90a33c8f0c8da77e87
JM14OK <jm14ok@cs.aau.dk@a2048-gc1-02.srv.aau.dk>
2024-01-16 14:39:28 +0100

    LexMa and Magic now use updated endpoint

--- benchmark.sh
+++ benchmark.sh
@@ -20,32 +20,69 @@ docker stop searx
 docker stop fuseki-service
 
 # Magic
-docker run --rm --network evaluation -d -v ${PWD}/baselines/magic/tdb_dbp_2016/:/tdb -v ${PWD}/baselines/magic/lucene_dbp_2016/:/lucene -p 7000:7000 -e MEM=200g --name kg-lookup-service kg-lookup
+docker run -it --rm -d --network kg-lookup-network \
+           -v ${PWD}/baselines/magic/lucene_dbp_2016/:/lucene \
+           -p 7000:7000 \
+           -e MEM=200g \
+           -e GRAPH=dbp_2016 \
+           -e VIRTUOSO=$(docker exec vos bash -c "hostname -I") \
+           --name kg-lookup-service kg-lookup
 sleep 2m
-docker run --rm --network evaluation -e ENDPOINT=${IP} -e KG="dbp-10-2016" -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/baselines/magic/:/hdt -v ${PWD}/results:/results magic
+docker run --rm --network kg-lookup-network -e ENDPOINT=${IP} -e KG="dbp-10-2016" -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/baselines/magic/:/hdt -v ${PWD}/results:/results magic
 docker stop kg-lookup-service
 
-docker run --rm --network evaluation -d -v ${PWD}/baselines/magic/tdb_dbp_2022/:/tdb -v ${PWD}/baselines/magic/lucene_dbp_2022/:/lucene -p 7000:7000 -e MEM=200g --name kg-lookup-service kg-lookup
+docker run -it --rm -d --network kg-lookup-network \
+           -v ${PWD}/baselines/magic/lucene_dbp_2022/:/lucene \
+           -p 7000:7000 \
+           -e MEM=200g \
+           -e GRAPH=dbp_2022 \
+           -e VIRTUOSO=$(docker exec vos bash -c "hostname -I") \
+           --name kg-lookup-service kg-lookup
 sleep 2m
-docker run --rm --network evaluation -e ENDPOINT=${IP} -e KG="dbp-12-2022" -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/baselines/magic/:/hdt -v ${PWD}/results:/results magic
+docker run --rm --network kg-lookup-network -e ENDPOINT=${IP} -e KG="dbp-12-2022" -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/baselines/magic/:/hdt -v ${PWD}/results:/results magic
 docker stop kg-lookup-service
 
-docker run --rm --network evaluation -d -v ${PWD}/baselines/lexma/tdb_wd/:/tdb -v ${PWD}/baselines/lexma/lucene_wd/:/lucene -p 7000:7000 -e MEM=200g --name kg-lookup-service kg-lookup
+docker run -it --rm -d --network kg-lookup-network \
+           -v ${PWD}/baselines/lexma/lucene_wd/:/lucene \
+           -p 7000:7000 \
+           -e MEM=200g \
+           -e GRAPH=wd \
+           -e VIRTUOSO=$(docker exec vos bash -c "hostname -I") \
+           --name kg-lookup-service kg-lookup
 sleep 2m
 docker run --rm --network evaluation -e ENDPOINT=${IP} -e KG="wd" -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/baselines/magic/:/hdt -v ${PWD}/results:/results magic
-
-# LexMa
-docker run --rm --network evaluation -d -v ${PWD}/baselines/magic/tdb_dbp_2016/:/tdb -v ${PWD}/baselines/magic/lucene_dbp_2016/:/lucene -p 7000:7000 -e MEM=200g --name kg-lookup-service kg-lookup
-sleep 2m
-docker run --rm --network evaluation -e ENDPOINT=${IP} -e KG=dbp_16 -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/results:/results lexma
 docker stop kg-lookup-service
 
-docker run --rm --network evaluation -d -v ${PWD}/baselines/magic/tdb_dbp_2022/:/tdb -v ${PWD}/baselines/magic/lucene_dbp_2022/:/lucene -p 7000:7000 -e MEM=200g --name kg-lookup-service kg-lookup
+# LexMa
+docker run -it --rm -d --network kg-lookup-network \
+           -v ${PWD}/baselines/magic/lucene_dbp_2016/:/lucene \
+           -p 7000:7000 \
+           -e MEM=200g \
+           -e GRAPH=dbp_2016 \
+           -e VIRTUOSO=$(docker exec vos bash -c "hostname -I") \
+           --name kg-lookup-service kg-lookup
 sleep 2m
-docker run --rm --network evaluation -e ENDPOINT=${IP} -e KG=dbp_22 -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/results:/results lexma
+docker run --rm --network kg-lookup-network -e ENDPOINT=${IP} -e KG=dbp_16 -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/results:/results lexma
+docker stop kg-lookup-service
+
+docker run -it --rm -d --network kg-lookup-network \
+           -v ${PWD}/baselines/magic/lucene_dbp_2022/:/lucene \
+           -p 7000:7000 \
+           -e MEM=200g \
+           -e GRAPH=dbp_2022 \
+           -e VIRTUOSO=$(docker exec vos bash -c "hostname -I") \
+           --name kg-lookup-service kg-lookup
+sleep 2m
+docker run --rm --network kg-lookup-network -e ENDPOINT=${IP} -e KG=dbp_22 -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/results:/results lexma
 docker stop kg-lookip-service
 
-docker run --rm --network evaluation -d -v ${PWD}/baselines/lexma/tdb_wd/:/tdb -v ${PWD}/baselines/lexma/lucene_wd/:/lucene -p 7000:7000 -e MEM=200g --name kg-lookup-service kg-lookup
+docker run -it --rm -d --network kg-lookup-network \
+           -v ${PWD}/baselines/lexma/lucene_wd/:/lucene \
+           -p 7000:7000 \
+           -e MEM=200g \
+           -e GRAPH=wd \
+           -e VIRTUOSO=$(docker exec vos bash -c "hostname -I") \
+           --name kg-lookup-service kg-lookup
 sleep 2m
-docker run --rm --network evaluation -e ENDPOINT=${IP} -e KG=wd -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/results:/results lexma
+docker run --rm --network kg-lookup-network -e ENDPOINT=${IP} -e KG=wd -v ${PWD}/benchmarks:/benchmarks -v ${PWD}/results:/results lexma
 docker stop kg-lookup-service
