c643db987959347a461708199f0e15c0d69e1ad5
JM14OK <jm14ok@cs.aau.dk@a2048-gc1-02.srv.aau.dk>
2024-01-20 17:13:42 +0100

    Temp

--- setup-baselines.sh
+++ setup-baselines.sh
@@ -18,40 +18,44 @@ mkdir -p results/keyword-kg-linker/
 mkdir -p results/lexma/
 mkdir -p results/magic/
 
-docker pull searx/searx
+#docker pull searx/searx
 docker network inspect ${NETWORK} >/dev/null 2>&1 || docker network create ${NETWORK}
 
-docker build -t fuseki -f ${BBW}fuseki.dockerfile ${BBW}
-docker build -t bbw -f ${BBW}bbw.dockerfile ${BBW}
-docker build -t magic -f ${MAGIC}magic.dockerfile ${MAGIC}
-docker build -t lexma -f ${LEXMA}lexma.dockerfile ${LEXMA}
-docker build -t emblookup -f ${EMBLOOKUP}emblookup.dockerfile ${EMBLOOKUP}
+#docker build -t fuseki -f ${BBW}fuseki.dockerfile ${BBW}
+#docker build -t bbw -f ${BBW}bbw.dockerfile ${BBW}
+#docker build -t magic -f ${MAGIC}magic.dockerfile ${MAGIC}
+#docker build -t lexma -f ${LEXMA}lexma.dockerfile ${LEXMA}
+#docker build -t emblookup -f ${EMBLOOKUP}emblookup.dockerfile ${EMBLOOKUP}
 
-git clone https://github.com/MrPekar98/kg-lookup.git
-cd kg-lookup/
-echo "Loading DBpedia 2016..."
-./load.sh ../setup/tough_tables/dbpedia/ dbp_2016
-docker stop vos
-rm -rf import/
+#git clone https://github.com/MrPekar98/kg-lookup.git
+#cd kg-lookup/
+#echo "Loading DBpedia 2016..."
+#./load.sh ../setup/tough_tables/dbpedia/ dbp_2016
+#docker stop vos
+#rm -rf import/
 
-echo "Loading DBpedia 2022..."
-./load.sh ../setup/kg/dbpedia/ dbp_2022
-docker stop vos
-rm -rf import/
+#echo "Loading DBpedia 2022..."
+#./load.sh ../setup/kg/dbpedia/ dbp_2022
+#docker stop vos
+#rm -rf import/
 
-echo "Loading Wikidata..."
-./load.sh ../setup/kg/sub_wikidata/ wd
-rm -rf import/
+#echo "Loading Wikidata..."
+#./load.sh ../setup/tough_tables/wikidata/ wd
+#rm -rf import/
 
-docker build -t kg-lookup .
-cd ..
+#docker build -t kg-lookup . --no-cache
+#cd ..
 
-mkdir -p baselines/magic/lucene_dbp_2016/
-mkdir -p baselines/magic/lucene_dbp_2022/
-mkdir -p baselines/lexma/lucene_wd/
+LUCENE_DBP_2016="baselines/magic/lucene_dbp_2016/"
+LUCENE_DBP_2022="baselines/magic/lucene_dbp_2022/"
+LUCENE_WD="baselines/lexma/lucene_wd/"
 
-docker run -it -d --rm --network kg-lookup-network \
-           -v ${PWD}/baselines/magic/lucene_dbp_2016/:/lucene \
+mkdir -p ${LUCENE_DBP_2016}
+mkdir -p ${LUCENE_DBP_2022}
+mkdir -p ${LUCENE_WD}
+
+docker run --rm -d --network kg-lookup-network \
+           -v ${PWD}/${LUCENE_DBP_2016}:/lucene \
            -p 7000:7000 \
            -e MEM=200g \
            -e GRAPH=dbp_2016 \
@@ -62,9 +66,8 @@ curl http://localhost:7000/index
 docker stop kg-lookup-service
 sleep 2m
 
-docker run -it -d --rm --network kg-lookup-network \
-           -v ${PWD}/baselines/magic/lucene_dbp_2022/:/lucene \
-           -p 7000:7000 \
+docker run --rm -d --network kg-lookup-network \
+           -v ${PWD}/${LUCENE_DBP_2022}:/lucene \
            -e MEM=200g \
            -e GRAPH=dbp_2022 \
            -e VIRTUOSO=$(docker exec vos bash -c "hostname -I") \
@@ -74,8 +77,8 @@ curl http://localhost:7000/index
 docker stop kg-lookup-service
 sleep 2m
 
-docker run -it -d --rm --network kg-lookup-network \
-           -v ${PWD}/baselines/lexma/lucene_wd/:/lucene \
+docker run --rm -d --network kg-lookup-network \
+           -v ${PWD}/${LUCENE_WD}:/lucene \
            -p 7000:7000 \
            -e MEM=200g \
            -e GRAPH=wd \
