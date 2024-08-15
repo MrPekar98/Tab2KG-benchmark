97fe4a702b8c86b00b0303d77d76032697a38964
MrPekar98 <jm14ok@cs.aau.dk@a256-gc1-19.srv.aau.dk>
2024-08-15 09:44:49 +0200

    Updated candidate generation experiments

--- baselines/lexma/experiments/candidates.sh
+++ baselines/lexma/experiments/candidates.sh
@@ -5,27 +5,6 @@ set -e
 BENCHMARK_DIR="/benchmarks/"
 RESULT_DIR="/results/"
 
-if [[ "${KG}" = "wd" ]]
-then
-    # tFood
-    mkdir -p ${RESULT_DIR}lexma/tfood_candidates/
-    CORPUS="${BENCHMARK_DIR}semtab/tfood/horizontal/tables_subset/"
-
-    if [[ -d ${CORPUS} ]]
-    then
-        python lexma_candidates.py wikidata ${CORPUS} ${RESULT_DIR}lexma/tfood_candidates/ 10 "http://${ENDPOINT}:7000/"
-    fi
-
-    # Wikitables 2013 - Wikidata
-    mkdir -p ${RESULT_DIR}lexma/wikitables_2013_wd_candidates/
-    CORPUS="${BENCHMARK_DIR}wikitables_2013/tables_subset/"
-
-    if [[ -d ${CORPUS} ]]
-    then
-        python lexma_candidates.py wikidata ${CORPUS} ${RESULT_DIR}lexma/wikitables_2013_wd_candidates/ 10 "http://${ENDPOINT}:7000/"
-    fi
-fi
-
 if [[ "${KG}" = "dbp_16" ]]
 then
     # Tough Tables - DBpedia
@@ -35,5 +14,17 @@ then
     if [[ -d ${CORPUS} ]]
     then
         python lexma_candidates.py dbpedia ${CORPUS} ${RESULT_DIR}lexma/toughtables_dbp_candidates/ 10 "http://${ENDPOINT}:7000/"
+    fi
+fi
+
+if [[ "${KG}" = "dbp_22" ]]
+then
+    # Wikitables 2019 - DBpedia
+    mkdir -p ${RESULT_DIR}lexma/wikitables_2019_dbp_candidates/
+    CORPUS="${BENCHMARK_DIR}wikitables_2019/tables_subset/"
+
+    if [[ -d ${CORPUS} ]]
+    then
+        python lexma_candidates.py dbpedia ${CORPUS} ${RESULT_DIR}lexma/wikitables_2019_dbp_candidates/ 10 "http://${ENDPOINT}:7000/"
     fi
 fi
