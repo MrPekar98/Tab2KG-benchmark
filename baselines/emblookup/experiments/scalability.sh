a9a86911ea1e615191393e60adec12479469753c
JM14OK <jm14ok@cs.aau.dk@a2048-gc1-02.srv.aau.dk>
2024-08-22 10:09:21 +0200

    Save results

--- baselines/emblookup/experiments/scalability.sh
+++ baselines/emblookup/experiments/scalability.sh
@@ -14,6 +14,7 @@ if [[ -d ${TOUGH_TABLES_WD} ]]
 then
     mkdir -p /results/emblookup/toughtables_wd_scalability
     python3 main.py "${TOUGH_TABLES_WD}" "${DATA_DIR}aliases/alias_wd.ttl" "${DATA_DIR}index_mappings/kg_index_name_mapping_wd.csv" "${DATA_DIR}aliases_processed/aliases_processed_wd.csv" -h -T ${TIME_LIMIT}
+    mv results.csv /results/emblookup/toughtables_wd_scalability
 fi
 
 # Wikitables
@@ -21,4 +22,5 @@ if [[ -d ${WIKITABLES} ]]
 then
     mkdir -p /results/emblookup/wikitables_dbp_2013_scalability
     python3 main.py "${WIKITABLES}" "${DATA_DIR}aliases/alias_dbp12-2022.ttl" "${DATA_DIR}index_mappings/kg_index_name_mapping_dbp12-2022.csv" "${DATA_DIR}aliases_processed/aliases_processed_dbp12-2022.csv" -h -T ${TIME_LIMIT}
+    mv results.csv /results/emblookup/wikitables_dbp_2013_scalability
 fi
