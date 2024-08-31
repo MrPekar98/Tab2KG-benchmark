81225b8cef2f1f09aab594ed2e03995e8693f30b
MrPekar98 <jm14ok@cs.aau.dk@a256-gc1-19.srv.aau.dk>
2024-08-31 12:08:54 +0200

    Only get candidates

--- baselines/emblookup/experiments/candidates.sh
+++ baselines/emblookup/experiments/candidates.sh
@@ -8,11 +8,11 @@ DATA_DIR="data/"
 # Tough Tables - DBpedia
 mkdir -p /results/emblookup/toughtables_dbp_candidates/
 CORPUS="${BENCHMARK_DIR}toughtables/dbpedia/tables_subset/"
-python3 main.py ${CORPUS} "${DATA_DIR}aliases/alias_dbp10-2016.ttl" "${DATA_DIR}index_mappings/kg_index_name_mapping_dbp10-2016.csv" "${DATA_DIR}aliases_processed/aliases_processed_dbp10-2016.csv" -h
+python3 main.py ${CORPUS} "${DATA_DIR}aliases/alias_dbp10-2016.ttl" "${DATA_DIR}index_mappings/kg_index_name_mapping_dbp10-2016.csv" "${DATA_DIR}aliases_processed/aliases_processed_dbp10-2016.csv" -h -c
 mv results.csv /results/emblookup/toughtables_dbp_candidates/
 
 # Wikitables 2019 - DBpedia
 mkdir -p /results/emblookup/wikitables_2019_dbp_candidates/
 CORPUS="${BENCHMARK_DIR}wikitables_2019/tables_subset/"
-python3 main.py ${CORPUS} "${DATA_DIR}aliases/alias_dbp12-2022.ttl" "${DATA_DIR}index_mappings/kg_index_name_mapping_dbp12-2022.csv" "${DATA_DIR}aliases_processed/aliases_processed_dbp12-2022.csv" -h
+python3 main.py ${CORPUS} "${DATA_DIR}aliases/alias_dbp12-2022.ttl" "${DATA_DIR}index_mappings/kg_index_name_mapping_dbp12-2022.csv" "${DATA_DIR}aliases_processed/aliases_processed_dbp12-2022.csv" -h -c
 mv results.csv /results/emblookup/wikitables_2019_dbp_candidates/
