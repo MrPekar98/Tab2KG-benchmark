e103062c2e1a26605674c0e05dd9bdf6129df1ad
MrPekar98 <jm14ok@cs.aau.dk@a256-gc1-19.srv.aau.dk>
2024-09-07 20:47:46 +0200

    MAGIC candidate generation experiment

--- baselines/magic/experiments/candidates.sh
+++ baselines/magic/experiments/candidates.sh
@@ -14,11 +14,20 @@ then
     python3 main_candidates.py dbpedia ${CORPUS} ${RESULT_DIR}magic/tough_tables_dbp_candidates/ ${ENDPOINT}
 fi
 
+# Wikitables 2013 - DBpedia
+CORPUS="${BENCHMARK_DIR}wikitables_2013/tables_subset/"
+
+if [[ "${KG}" = "dbp-12-2022" && -d ${CORPUS} ]]
+then
+    mkdir -p ${RESULT_DIR}magic/wikitables-2013_dbp_candidates/
+    python3 main_candidates.py dbpedia ${CORPUS} ${RESULT_DIR}magic/wikitables-2013_dbp_candidates/ ${ENDPOINT}
+fi
+
 # Wikitables 2019 - DBpedia
 CORPUS="${BENCHMARK_DIR}wikitables_2019/tables_subset/"
 
 if [[ "${KG}" = "dbp-12-2022" && -d ${CORPUS} ]]
 then
     mkdir -p ${RESULT_DIR}magic/wikitables-2019_dbp_candidates/
-    python3 main_candidates.py wikidata ${CORPUS} ${RESULT_DIR}magic/wikitables-2019_dbp_candidates/ ${ENDPOINT}
+    python3 main_candidates.py dbpedia ${CORPUS} ${RESULT_DIR}magic/wikitables-2019_dbp_candidates/ ${ENDPOINT}
 fi
