ae3419f864058cdef140bdefce99e2cf5f343f37
JM14OK <jm14ok@cs.aau.dk@a2048-gc1-02.srv.aau.dk>
2024-09-19 11:23:13 +0200

    Scalability experiment on WT19

--- baselines/lexma/experiments/scalability.sh
+++ baselines/lexma/experiments/scalability.sh
@@ -3,9 +3,9 @@
 set -e
 
 BENCHMARK_DIR="/benchmarks/"
-TOUGH_TABLES_DBP=${BENCHMARK_DIR}toughtables/dbpedia/tables/
 TOUGH_TABLES_WD=${BENCHMARK_DIR}toughtables/wikidata/tables/
-WIKITABLES=${BENCHMARK_DIR}wikitables_2013/tables/
+WIKITABLES_2013=${BENCHMARK_DIR}wikitables_2013/tables/
+WIKITABLES_2019=${BENCHMARK_DIR}wikitables_2019/tables/
 SLEEP=8h
 
 # ToughTables - Wikidata
@@ -20,13 +20,25 @@ then
     kill -9 ${PID}
 fi
 
-# Wikitables
-if [[ "${KG}" = "dbp_22" && -d ${WIKITABLES} ]]
+# Wikitables 2013
+if [[ "${KG}" = "dbp_22" && -d ${WIKITABLES_2013} ]]
 then
     RESULTS="/results/lexma/wikitables_dbp_2013_scalability"
     mkdir -p ${RESULTS}
 
-    python lexma.py dbpedia "${WIKITABLES}" ${RESULTS} 10 "http://${ENDPOINT}:7000/" &
+    python lexma.py dbpedia "${WIKITABLES_2013}" ${RESULTS} 10 "http://${ENDPOINT}:7000/" &
+    PID=$1
+    sleep ${SLEEP}
+    kill -9 ${PID}
+fi
+
+# Wikitables 2019
+if [[ "${KG}" = "dbp_22" && -d ${WIKITABLES_2019} ]]
+then
+    RESULTS="/results/lexma/wikitables_dbp_2019_scalability"
+    mkdir -p ${RESULTS}
+
+    python lexma.py dbpedia "${WIKITABLES_2019}" ${RESULTS} 10 "http://${ENDPOINT}:7000/" &
     PID=$1
     sleep ${SLEEP}
     kill -9 ${PID}
