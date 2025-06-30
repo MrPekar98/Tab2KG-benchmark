0d0f92698aa76909e8aca50e3c0ad35698b7c515
JM14OK <jm14ok@cs.aau.dk@a2048-gc1-02.srv.aau.dk>
2025-06-30 11:12:26 +0200

    Candidate generation experiment

--- baselines/citysti/citysti.dockerfile
+++ baselines/citysti/citysti.dockerfile
@@ -6,7 +6,10 @@ RUN git clone https://github.com/dylanlty/CitySTI-2024.git
 
 WORKDIR CitySTI-2024/
 RUN pip install -r requirements.txt
+RUN rm lookup.py
+ADD lookup.py .
 ADD gemini_main.py .
+ADD gemini_candidates.py .
 ADD evaluate.sh .
 ADD experiments/ .
 
--- baselines/citysti/experiments/candidates.sh
+++ baselines/citysti/experiments/candidates.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
 set -e
+
+BENCHMARK_DIR="/benchmarks/"
+RESULT_DIR="/results/"
+
+if [[ "${KG}" = "wd" ]]
+then
+    # Tough Tables - Wikidata
+    mkdir -p ${RESULT_DIR}citysti/toughtables_wd_candidates/
+    CORPUS="${BENCHMARK_DIR}toughtables/wikidata/tables_subset/"
+    python gemini_candidates.py ${API_KEY} wikidata ${CORPUS} ${RESULT_DIR}citysti/toughtables_wd_candidates/ ${ENDPOINT}
+
+    # HardTables
+    mkdir -p ${RESULT_DIR}citysti/hardtables_candidates/
+    CORPUS="${BENCHMARK_DIR}semtab/HardTables/tables_subset/"
+    python gemini_candidates.py ${API_KEY} wikidata ${CORPUS} ${RESULT_DIR}citysti/hardtables_candidates/ ${ENDPOINT}
+
+    # Wikitables 2019 - Wikidata
+    mkdir -p ${RESULT_DIR}citysti/wikitables_2019_wd_candidates/
+    CORPUS="${BENCHMARK_DIR}wikitables_2019/tables_subset/"
+    python gemini_candidates.py ${API_KEY} wikidata ${CORPUS} ${RESULT_DIR}citysti/wikitables_2019_wd_candidates/ ${ENDPOINT}
+fi
